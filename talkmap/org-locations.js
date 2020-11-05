c8cbfc6ad3012481c49ff1cadeeefd35b7e8f643
Victoria McDonald <49625194+torimcd@users.noreply.github.com>
2020-11-05 14:01:47 -0800

    update talk locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Victoria BC, CANADA",
+    48.4284,
+    -123.3656
   ],
   [
     "London, UK",
@@ -10,13 +10,53 @@ var addressPoints = [
     -0.1276473
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Edinburgh, UK",
+    55.9533,
+    -3.1883
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Rotterdam, NL",
+    51.9244,
+    4.4777
+  ],
+  [
+    "Paris, FR",
+    48.8566,
+    2.3522
+  ],
+  [
+    "Oslo, NO",
+    59.9139,
+    10.7522
+  ],
+  [
+    "Bangkok, TH",
+    13.7563,
+    100.5018
+  ],
+  [
+    "Copenhagen, DK",
+    55.6761,
+    12.5683
+  ],
+  [
+    "Dubai, UAE",
+    25.2048,
+    55.2708
+  ],
+  [
+    "Kuwait City, KW",
+    29.3759,
+    47.9774
+  ],
+  [
+    "San Diego, CA, USA",
+    32.7157,
+    -117.1611
+  ],
+  [
+    "Fort Worth, TX, USA",
+    32.7555,
+    -97.3308
   ]
 ];
